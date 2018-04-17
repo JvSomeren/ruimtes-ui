1b2c711ef899e5318b0f8261b90758969182c2e2
Joost van Someren <joost.v.someren@gmail.com>
2018-04-17 21:56:04 +0200

    Import Buefy in application

--- src/main.js
+++ src/main.js
@@ -2,12 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './route/router.js'
 import store from './store'
+import Buefy from 'buefy'
 import FullCalendar from 'vue-full-calendar'
 
+import 'buefy/lib/buefy.css'
 import "fullcalendar-scheduler";
 import "fullcalendar/dist/fullcalendar.min.css";
 import "fullcalendar-scheduler/dist/scheduler.min.css";
 
+Vue.use(Buefy);
 Vue.use(FullCalendar);
 Vue.config.productionTip = false;
 
