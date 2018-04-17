3e2f49ae073761e7a6eb07a8591927e8cf33fb12
Joost van Someren <joost.v.someren@gmail.com>
2018-04-17 21:57:02 +0200

    Add Navbar component

---
+++ src/components/Navbar.vue
@@ -0,0 +1,60 @@
+<template>
+  <nav class="navbar is-primary is-fixed-top" role="navigation" aria-label="main navigation">
+
+    <div class="navbar-brand">
+      <router-link class="navbar-item" to="/">
+        <img src="https://bulma.io/images/bulma-logo.png" alt="Bulma: a modern CSS framework based on Flexbox" width="112" height="28">
+      </router-link>
+      <div class="navbar-burger burger" data-target="navbarExampleTransparentExample">
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+    </div>
+
+    <div class="navbar-menu">
+      <div class="navbar-start">
+        Start
+      </div>
+
+      <div class="navbar-end">
+        <div class="navbar-item">
+          <div class="is-field">
+            <p class="control">
+              <router-link class="button is-primary" to="/settings">
+                <span class="icon">
+                  <i class="mdi mdi-settings"></i>
+                </span>
+                <span>Instellingen</span>
+              </router-link>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </nav>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      
+    }
+  },
+  computed: {
+
+  },
+  methods: {
+
+  },
+  created() {
+
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
--- src/main.js
+++ src/main.js
@@ -16,8 +16,12 @@ Vue.config.productionTip = false;
 
 import Calendar from './components/Calendar';
 import EventModal from './components/EventModal';
+import Navbar from './components/Navbar';
+
 Vue.component('calendar', Calendar);
 Vue.component('event-modal', EventModal);
+Vue.component('navbar', Navbar);
+
 let Main = Vue.component('app', App);
 
 Main = new Main({
